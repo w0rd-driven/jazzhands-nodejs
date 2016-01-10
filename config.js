a84250fce6538b00e185b920626ee0c38e92494f
Jeremy Brayton <jbrayton@rethinkgroup.org>
2016-01-10 16:37:08 -0500

    Using mkdirp for recursive directory creation. We sadly have to make sure the output directory is created before we try to put solutions there.

--- config.js
+++ config.js
@@ -2,3 +2,10 @@
  * Created by jbrayton on 1/10/16.
  */
 
+var config = {
+    buildDirectory: "../..",
+    outputDirectory: "/build/node.js/",
+    outputFile: "output.txt"
+};
+
+module.exports = config;
